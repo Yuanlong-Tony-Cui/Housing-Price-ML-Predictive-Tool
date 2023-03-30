484ea16a49315ad8881f054ba3a42da94da7d5fa
Yuanlong-Tony-Cui <yuanlongcui0511@gmail.com>
2023-03-30 17:10:29 -0400

    Visualization of input dataset

--- Processing/main.py
+++ Processing/main.py
@@ -1,7 +1,11 @@
-from dist_to_campus import dist_to_campus
-from dist_to_grocery import dist_to_grocery
-import json
+# from dist_to_campus import dist_to_campus
+# from dist_to_grocery import dist_to_grocery
+# import json
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
+"""
 # Load the input data from JSON file
 with open('Processing/sample_addresses.json', 'r') as f:
     input_data = json.load(f)
@@ -14,3 +18,21 @@ distances = dist_to_campus(addresses,False)
 print(distances)
 distances = dist_to_grocery(addresses,False)
 print(distances)
+"""
+
+# Load data into a dataframe:
+data = open("Processing/sample_dataset.csv", "rb")
+df = pd.read_csv(data)
+# Summary:
+print(df.head(5))
+print(df.shape)
+print(df.ndim)
+print(df.info())
+print(df.describe())
+# Plots:
+plt.hist(df['price'].to_numpy(), bins=10, edgecolor='black')
+plt.xlabel('Price')
+plt.ylabel('Count')
+plt.title('Price Distribution')
+plt.tight_layout()
+plt.show()
